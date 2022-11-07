652f87803db35037d4e367d32787feb37e7dc73a
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2022-11-08 00:20:23 +0200

    added service & api Marvel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
+import MarvelService from './services/MarvelService'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+ 
