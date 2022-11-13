d3c2fc83aa0b79f5866b46bb839b5cdbf04a7b44
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2022-11-14 00:10:58 +0200

    added logic dynamic loading charList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
-
-import { Component } from 'react';
+import {
+    Component
+} from 'react';
 import './charList.scss';
 import Spinner from '../Spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -11,22 +12,41 @@ class CharList extends Component {
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
+
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.marvelService.getAllCharacters()
+        this.onRequest();
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading();
+        this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
             .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemLoading: true
         })
+    }
+
+    onCharListLoaded = (newCharList) => {
+
+        
+        this.setState(({ offset, charList }) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9
+        }))
     }
 
     onError = () => {
@@ -40,46 +60,52 @@ class CharList extends Component {
     // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
         const items = arr.map((item) => {
-            let imgStyle = { 'objectFit': 'cover' };
+            let imgStyle = {
+                'objectFit': 'cover'
+            };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = { 'objectFit': 'unset' };
+                imgStyle = {
+                    'objectFit': 'unset'
+                };
             }
 
             return (
-                <li
-                    className="char__item"
+                <li className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}
-                    >
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle} />
-                    <div className="char__name">{item.name}</div>
+                    onClick={() => this.props.onCharSelected(item.id)} >
+                    <img src={item.thumbnail}
+                        alt={item.name}
+                        style={imgStyle} />
+                    <div className="char__name" > {item.name}
+                    </div>
                 </li>
             )
         });
         // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
-            <ul className="char__grid">
-                {items}
-            </ul>
+            <ul className="char__grid" > {items} </ul>
         )
     }
 
     render() {
 
-        const { charList, loading, error } = this.state;
+        const { charList, loading, error, newItemLoading, offset } = this.state;
         const items = this.renderItems(charList);
 
-        const errorMessage = error ? <ErrorMessage /> : null;
-        const spinner = loading ? <Spinner /> : null;
+        const errorMessage = error ? < ErrorMessage /> : null;
+        const spinner = loading ? < Spinner /> : null;
         const content = !(loading || error) ? items : null;
-        
+
         return (
-            <div className="char__list">
+            <div className="char__list" >
                 {errorMessage}
-                {spinner} 
+                {spinner}
                 {content}
-                <button className="button button__main button__long">
-                    <div className="inner">load more</div>
+                <button 
+                    className="button button__main button__long" 
+                    disabled={newItemLoading}
+                    onClick={() => this.onRequest(offset)}>
+                    <div className="inner" > load more </div>
                 </button>
             </div>
         )
