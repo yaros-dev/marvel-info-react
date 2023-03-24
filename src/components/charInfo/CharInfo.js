d190dccc21a31db9da454f653d410d57c79f5241
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-03-24 11:04:17 +0200

    fixed bug path

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import './charInfo.scss';
 import useMarvelService from '../../services/MarvelService';
