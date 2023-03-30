3046d2b1bf66a1bd6974523dedd8dda23db0554d
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-03-30 22:50:29 +0300

    create utils folder and create fn setContent

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -2,9 +2,7 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './charInfo.scss';
 import useMarvelService from '../../services/MarvelService';
-import Spinner from '../Spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import Skeleton from '../skeleton/Skeleton';
+import setContent from '../../utils/setContent';
 
 
 const CharInfo = (props) => {
@@ -40,45 +38,19 @@ const CharInfo = (props) => {
         setShow(true);
     }
 
-    const setContent = (process, charInfo) => {
-        switch (process) {
-            case 'waiting':
-                return <Skeleton />;
-                break;
-            case 'loading':
-                return <Spinner />;
-                break;
-            case 'confirmed':
-                return <View char={charInfo} />;
-                break;
-            case 'error':
-                return <ErrorMessage />;
-                break;
-            default:
-                throw new Error('Unexpected process state')
-                break;
-        }
-    }
 
-
-    // const skeleton = charInfo || loading || error ? null : <Skeleton />;
-    // const errorMessage = error ? <ErrorMessage /> : null;
-    // const spinner = loading ? <Spinner /> : null;
-    // const content = !(loading || error || !charInfo) ? <View char={charInfo} /> : null
 
     return (
-
         <div className="char__info">
-            {setContent(process, charInfo)}
+            {setContent(process, View, charInfo)}
         </div>
-
     )
 
 }
 
-const View = ({ char }) => {
+const View = ({ data }) => {
 
-    const { name, description, thumbnail, homepage, wiki, comics } = char;
+    const { name, description, thumbnail, homepage, wiki, comics } = data;
 
     let imgStyle = { 'objectFit': 'cover' };
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
@@ -87,7 +59,6 @@ const View = ({ char }) => {
 
     return (
         <>
-
             <div className="char__basics">
                 <img src={thumbnail} alt={name} style={imgStyle} />
                 <div>
@@ -108,21 +79,15 @@ const View = ({ char }) => {
 
             <ul className="char__comics-list">
                 {
-
                     comics.map((item, i) => {
-
                         // eslint-disable-next-line
                         if (i > 9) return;
                         return (
-
                             <li key={i} className="char__comics-item" >
                                 {item.name}
                             </li>
-
                         )
-                    }
-                    )
-
+                    })
                 }
 
             </ul>
