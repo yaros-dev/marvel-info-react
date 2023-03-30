6668856cc09a0d524cc18ce69e18bf58c162494e
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-03-30 23:52:20 +0300

    new logic of RandomChar display

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -4,13 +4,13 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
-
+import setContent from '../../utils/setContent';
 
 const RandomChar = () => {
 
     const [char, setChar] = useState({});
 
-    const { loading, error, getCharacter, clearError } = useMarvelService();
+    const { loading, error, getCharacter, clearError, process, setProcess } = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -30,18 +30,15 @@ const RandomChar = () => {
         clearError();
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         getCharacter(id)
-            .then(onCharLoaded);
+            .then(onCharLoaded)
+            .then(() => setProcess('confirmed'));
     }
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? <View char={char} /> : null
+
     return (
         <>
             <div className="randomchar" >
-                {errorMessage}
-                {spinner}
-                {content}
+                {setContent(process, View, char)}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br />
@@ -60,8 +57,8 @@ const RandomChar = () => {
     )
 }
 
-const View = ({ char }) => {
-    const { name, description, thumbnail, homepage, wiki } = char;
+const View = ({ data }) => {
+    const { name, description, thumbnail, homepage, wiki } = data;
     const isImgChar = thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? true : false;
     const classNamesImgChar = `randomchar__img ${isImgChar && 'randomchar__not-img'} `;
 
